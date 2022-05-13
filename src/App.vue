37d79177291bea6b32b126ccb0733bc2a8f6e2a1
dansch1982 <dansch1982@gmail.com>
2022-05-13 10:04:39 +0200

    creation

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+	<AppHeader />
+	<main>Main</main>
+	<footer>Footer</footer>
+</template>
+
+<script>
+
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+	name: "App",
+	components: {AppHeader},
+};
+</script>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+#app {
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	grid-template-rows: max-content auto max-content;
+}
+</style>
