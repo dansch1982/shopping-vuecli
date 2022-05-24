57b7f4b44e4fe2bdab3f4bacfb0190d8da03ff82
dansch1982 <dansch1982@gmail.com>
2022-05-24 09:45:56 +0200

    Update websocket.js

--- src/utilities/websocket.js
+++ src/utilities/websocket.js
@@ -1,11 +1,25 @@
 /* const baseHOST = "http://localhost:1234/";
 const HOST = baseHOST.replace(/^http/, "ws"); */
 
-const HOST = location.origin.replace(/^http/, 'ws')
+const HOST = location.origin.replace(/^http/, "ws");
 const socket = new WebSocket(HOST);
 
 socket.addEventListener("open", () => {
 	console.log("Connection", true);
 });
+
+socket.on("open", heartbeat);
+socket.on("ping", heartbeat);
+socket.on("close", function clear() {
+	clearTimeout(this.pingTimeout);
+});
+
+function heartbeat() {
+	clearTimeout(this.pingTimeout);
+
+	this.pingTimeout = setTimeout(() => {
+		this.terminate();
+	}, 30000 + 1000);
+}
 
 export default socket;
